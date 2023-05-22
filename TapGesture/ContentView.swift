e4a58dfd948eda7d1f9d20eb94af114b8a5be257
Dio Tendean <tendeanclaudio1234@gmail.com>
2023-05-22 13:27:06 +0800

    first commit

--- TapGesture/ContentView.swift
+++ TapGesture/ContentView.swift
@@ -9,18 +9,83 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView{
+            VStack {
+                Feed().padding()
+                Spacer()
+            }
+            .navigationBarTitle("Instagram")
         }
-        .padding()
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct Feed : View {
+    
+    @State private var isTapped = false
+    
+    var body: some View {
+        VStack {
+            HStack {
+                Circle().frame(width: 25, height: 25)
+                Text("Claudio Tendean")
+                Spacer()
+                Image(systemName: "circle.grid.2x2")
+            }.padding()
+            
+            Image("g5")
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+                .frame(height: 400)
+                .clipped()
+            
+            HStack {
+                
+                if(!self.isTapped) {
+                    Image(systemName: "heart")
+                        .resizable()
+                        .frame(width: 25, height: 25)
+                        .onTapGesture {
+                            if(!self.isTapped) {
+                                self.isTapped = true
+                                print(self.isTapped)
+                            } else {
+                                self.isTapped = false
+                                print(self.isTapped)
+                            }
+                        }
+                } else {
+                    Image(systemName: "heart.fill")
+                        .resizable()
+                        .frame(width: 25, height: 25)
+                        .onTapGesture {
+                            if(!self.isTapped) {
+                                self.isTapped = true
+                                print(self.isTapped)
+                            } else {
+                                self.isTapped = false
+                                print(self.isTapped)
+                            }
+                        }.foregroundColor(Color.red)
+                }
+                
+                
+                Image(systemName: "message")
+                    .resizable()
+                    .frame(width: 25, height: 25)
+                Image(systemName: "paperplane")
+                    .resizable()
+                    .frame(width: 25, height: 25)
+                Spacer()
+                Image(systemName: "bookmark")
+                    .resizable()
+                    .frame(width: 25, height: 25)
+            }.padding()
+        }.border(Color.gray)
     }
 }
