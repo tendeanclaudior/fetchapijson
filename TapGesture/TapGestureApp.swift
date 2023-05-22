2682eaf696d7d4320282060e7a32e7ddf9641407
Dio Tendean <tendeanclaudio1234@gmail.com>
2023-05-22 09:47:02 +0800

    Initial Commit

---
+++ TapGesture/TapGestureApp.swift
@@ -0,0 +1,17 @@
+//
+//  TapGestureApp.swift
+//  TapGesture
+//
+//  Created by Claudio Tendean on 22/05/23.
+//
+
+import SwiftUI
+
+@main
+struct TapGestureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
