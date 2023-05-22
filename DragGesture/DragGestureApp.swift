63ef47e14dc61036fc0230873a138ddafd0d48d1
Dio Tendean <tendeanclaudio1234@gmail.com>
2023-05-22 10:22:52 +0800

    Initial Commit

---
+++ DragGesture/DragGestureApp.swift
@@ -0,0 +1,17 @@
+//
+//  DragGestureApp.swift
+//  DragGesture
+//
+//  Created by Claudio Tendean on 22/05/23.
+//
+
+import SwiftUI
+
+@main
+struct DragGestureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
